f151687dc4ab29deba18f8b7542e21a0fcb9b161
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-09-20 15:52:06 +0300

    remove log function

--- 03-This/6-9--4.js
+++ 03-This/6-9--4.js
@@ -9,12 +9,8 @@ function throttle(fn, interval) {
   };
 }
 
-function f(a) {
-  console.log(a);
-}
-
 // f1000 passes calls to f at maximum once per 1000 ms
-let f1000 = throttle(f, 1000);
+let f1000 = throttle(console.log, 1000);
 
 f1000(1); // shows 1
 f1000(2); // (throttling, 1000ms not out yet)
