6c11945ba2a6c266875582831bd12169a0cff9ef
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-11-06 14:19:09 +0200

    up to events

--- 05-Node/first-app/app.js
+++ 05-Node/first-app/app.js
@@ -1,3 +1,30 @@
-const log = require('./logger');
+const path = require('path');
 
-log('message');
+const pathObj = path.parse(__filename);
+// console.log(pathObj);
+
+const os = require('os');
+let totalMemory = os.totalmem();
+let freeMemory = os.freemem();
+
+// console.log('🚀 ~ file: app.js ~ line 8 ~ totalMemory', totalMemory);
+// console.log('🚀 ~ file: app.js ~ line 10 ~ freeMemory', freeMemory);
+
+const fs = require('fs');
+const files = fs.readdirSync('./');
+// console.log('🚀 ~ file: app.js ~ line 15 ~ files', files);
+
+// fs.readdir('$', function (err, files) {
+//   if (err) console.log('Error', err);
+//   else console.log('Result', files);
+// });
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+// Register a listener
+logger.on('messageLogged', (e) => {
+  console.log('Listener called', e);
+});
+
+logger.log('message');
--- 05-Node/first-app/logger.js
+++ 05-Node/first-app/logger.js
@@ -1,9 +1,16 @@
+const EventEmitter = require('events');
+
 const url = 'http://mylogger.io/log';
 
-function log(message) {
-  // Send an HTTP request
-  console.log(message);
+class Logger extends EventEmitter {
+  log(message) {
+    // Send an HTTP request
+    console.log(message);
+
+    // Raise an event
+    this.emit('messageLogged', { id: 1, url: 'http://' });
+  }
 }
 
-module.exports = log;
-module.exports.url = url;
+module.exports = Logger;
+// module.exports.url = url;
