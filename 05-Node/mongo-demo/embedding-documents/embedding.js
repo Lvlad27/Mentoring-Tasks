bbdce94a53994d35a00447be52f0bf28a3062bdc
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-11-25 00:16:38 +0200

    115. Embedding documents

--- 05-Node/mongo-demo/embedding-documents/embedding.js
+++ 05-Node/mongo-demo/embedding-documents/embedding.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose
+  .connect('mongodb://localhost/playground3')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+  .catch((err) => console.error('Could not connect to MongoDB...', err));
 
 const authorSchema = new mongoose.Schema({
   name: String,
@@ -12,23 +13,39 @@ const authorSchema = new mongoose.Schema({
 
 const Author = mongoose.model('Author', authorSchema);
 
-const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String
-}));
+const Course = mongoose.model(
+  'Course',
+  new mongoose.Schema({
+    name: String,
+    author: authorSchema
+  })
+);
 
 async function createCourse(name, author) {
   const course = new Course({
-    name, 
+    name,
     author
-  }); 
-  
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
+async function listCourses() {
   const courses = await Course.find();
   console.log(courses);
 }
 
-createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function updateAuthor(courseId) {
+  const course = await Course.updateOne(
+    { _id: courseId },
+    {
+      $set: {
+        'author.name': 'John Smith'
+      }
+    }
+  );
+}
+
+// createCourse('Node Course', new Author({ name: 'Mosh' }));
+updateAuthor('637fe2dfa5a2efe14a2f804c');
